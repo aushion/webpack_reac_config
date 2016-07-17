7fc05981f5a124532790463ddb1adbd25bc106e5
aushion <chenaosheng@163.com>
2016-07-17 19:06:21 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./webpack.config.dev');
+var WebpackDevServer = require("webpack-dev-server");
+config.entry.unshift("webpack-dev-server/client?http://localhost:3000/", "webpack/hot/dev-server");
+var compiler = webpack(config);
+var server = new WebpackDevServer(compiler, {
+  publicPath: config.output.publicPath,
+  hot: true,
+  stats:{colors:true}
+});
+server.listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Listening at http://localhost:3000/');
+});
---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+var node_modules_dir = path.resolve(__dirname, 'node_modules');
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './app/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: [node_modules_dir],
+      loader: 'babel',
+      include: path.join(__dirname, 'app')
+    }]
+  }
+};
